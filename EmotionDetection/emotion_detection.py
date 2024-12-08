1064d14529a4d316c7556300091335ad9f32339a
Nurgazy <nurgazy1@gmail.com>
2024-12-08 12:34:09 -0500

    emotion_detection.py updated

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -15,5 +15,23 @@ def emotion_detector(text_to_analyze):
     # Send a POST request to the API with the text and headers
     response = requests.post(url, json = myobj, headers=header)
 
-    # Return the response text from the API
-    return response.text
+    # Parsing the JSON response from the API
+    json_response = json.loads(response.text)
+
+    # Extracting the set of emotions
+    set_of_emotions = json_response['emotionPredictions'][0]['emotion']
+
+    # Defining max emotion
+    max_emotion = max(set_of_emotions, key=set_of_emotions.get)
+
+    # Appending dominant emotion
+    set_of_emotions['dominant_emotion'] = max_emotion
+
+    # Print out
+    print("{")
+    for key, value in set_of_emotions.items():
+        if isinstance(value, str):
+            print(f"    '{key}': '{value}',")
+        else:
+            print(f"    '{key}': {value},")
+    print("}")
