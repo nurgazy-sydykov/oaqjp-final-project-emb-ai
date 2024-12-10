17a7509bd348039a9890ae5e4402a0b7e3f349e3
Nurgazy <nurgazy1@gmail.com>
2024-12-10 11:11:13 -0500

    server.py is updated

--- server.py
+++ server.py
@@ -11,8 +11,10 @@ def detector():
     text_to_analyze = request.args.get('textToAnalyze')
     # Pass the text to the sentiment_analyzer function and store the response
     response = emotion_detector(text_to_analyze)
-    return response
-
+    text = json.dumps(response)
+    text = text[1:-2]
+    new_text = text.replace(', "dominant_emotion": "', '. The dominant emotion is ')
+    return f'For the given statement, the system response is {new_text}.'
 @app.route("/")
 def render_index_page():
     # Returning rendered template
