20112f9f944155a8e0dc2cded78f230099d57221
Nurgazy <nurgazy1@gmail.com>
2024-12-11 11:29:59 -0500

    final update

--- server.py
+++ server.py
@@ -1,12 +1,19 @@
+'''
+Emotion detection function server.py file. It deploys the server.
+'''
 import json
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
-#Initiate the flask app :
+# Initiate the flask app
 app = Flask('Emotion Detector')
 
 @app.route("/emotionDetector")
 def detector():
+    '''
+    This function takes some string as an argument {textToAnalyze}.
+    If an argument is empty, then it returns invalid error text.
+    '''
     # Retrieve the text to analyze from the request arguments
     text_to_analyze = request.args.get('textToAnalyze')
     # Pass the text to the sentiment_analyzer function and store the response
@@ -17,9 +24,12 @@ def detector():
         text = text.replace('}', '')
         new_text = text.replace(', "dominant_emotion": "', '. The dominant emotion is ')
         return f'For the given statement, the system response is {new_text}.'
-    return f'{text[1:-1]}'
+    return 'Invalid text! Please try again!'
 @app.route("/")
 def render_index_page():
+    '''
+    Rendering the index.html page
+    '''
     # Returning rendered template
     return render_template('index.html')
 
